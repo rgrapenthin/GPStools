2604187153e8b7b321450826ed0c7312121bba30
Ronni Grapenthin <rg@nmt.edu>
2015-08-12 19:41:24 -0600

    moved actual download code from get_site_log.py into util/util.py so this can be used in other parts of the code as a function call rather than a system call

--- get_site_log.py
+++ get_site_log.py
@@ -19,15 +19,9 @@
 #
 ###########################################################################
 
-import sys, getopt, os, shutil
-import urllib
-from classes.IGSLog import IGSLog
+import sys, getopt
+import util.util as util
 
-#hash table to map data center to log file url
-databases           = {}
-databases['unavco'] = 'ftp://data-out.unavco.org/pub/logs'
-databases['sopac']  = 'ftp://garner.ucsd.edu/pub/docs/site_logs'        
-databases['igs']    = 'ftp://igscb.jpl.nasa.gov/pub/station/finallog'
 site                = ''
 archive             = ''
 url                 = ''
@@ -42,25 +36,7 @@ OPTIONS:\n\
    -s, --site\t\t4 character site-id\n\
    -u, --url\t\tdatabase url.\n\n\
 Report bugs to rg@nmt.edu\n\
-" % (", ".join(databases.keys()))
-
-def retrieve_log(url):
-    sys.stdout.write('Retrieving '+url + '... ')
-    sys.stdout.flush()
-    
-    f=url.split('/')[-1]
-    
-    try:
-        urllib.urlretrieve(url, f)
-    except IOError:
-        sys.stdout.write("\t\tNothing found.\n")
-        sys.stdout.flush()
-        return False
-
-    sys.stdout.write("\tSuccess.\n")
-    sys.stdout.flush()
-
-    return f
+" % (", ".join(util.databases.keys()))
 
 ############# ############# ############# 
 ############# MAIN STUFF
@@ -101,76 +77,12 @@ if not site:
     sys.exit(2)
     
 if archive and not url:
-    if not databases.get(archive):
+    if not util.databases.get(archive):
         sys.stderr.write("Error: Archive %s not in local database, must provide URL\n" % (archive))
         usage()
         sys.exit(2)
-        
-##get going
-logfile = ''
 
-for a in databases.keys():
-    sys.stdout.write("Trying '"+a+"' archive: \t")
-
-    file_from = ''
-    file_url  = ''
-    
-    if a == 'unavco':
-        file_url  = databases[a]+'/'+site+'log.txt'
-        logfile   = retrieve_log(file_url)
-        if logfile:
-            file_from=a
-            break
-        
-    elif a == 'sopac':
-        file_url  = databases[a]+'/'+site+'.log.txt'
-        logfile  = retrieve_log(file_url)
-        if logfile:
-            file_from=a
-            break
-    #fallback:
-    #see if there's a file that contains sitename in its name 
-    #in the given directory at the server
-    else:
-        from ftplib import FTP
-        #get name of file at IGS
-        url = databases[a].split('//')[1].split('/')
-        ftp = FTP(url[0])
-        ftp.login()
-        ftp.cwd("/"+"/".join(url[1:]))
-        logfiles = ftp.nlst()
-        ftp.quit()
-
-        try:
-            sitefile = [filename for filename in logfiles if site in filename][-1]
-            file_url = databases[a]+'/'+sitefile
-            logfile  = retrieve_log(file_url)
-        except IndexError:
-            sys.stdout.flush()
-            sys.stderr.write("Couldn't find site `"+site.upper()+"'. Bye.\n")
-            sys.exit(2)
-
-        if logfile:
-            file_from=a
-            break
-
-if logfile:
-    local_log = site+"."+file_from+".log"
-    xml_log   = site+".xml"
-    log = IGSLog(logfile, site)
-    log.parse()
-    log.retrieved_from(file_from, file_url, local_log)
-    log.write(xml_log)
-    
-    #move files to site doc archive
-    dest = os.environ.get('GPS_SITE_DOC')
-    
-    if dest:
-        print "Moving logfiles to site-log archive '"+dest+"'"
-        #os.rename won't copy across partitions
-        shutil.move(logfile, dest+"/"+local_log)
-        shutil.move(xml_log, dest+"/"+xml_log)
-    else:
-        print "Environment variable 'GPS_SITE_DOC' not set. logfiles remain in current directory."
+      
+util.get_site_log(archive=archive, site=site, url=url)
 
 
