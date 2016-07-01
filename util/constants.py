b74ee95017f5125f94baa57f5e339b947d2a47ed
Ronni Grapenthin <rg@nmt.edu>
2016-07-01 17:35:31 -0600

    need these constants too, to avoid awkward strings all over the place

---
+++ util/constants.py
@@ -0,0 +1,23 @@
+
+TEQC_f_name         = 'filename'
+TEQC_f_format       = 'file format'
+TEQC_f_size         = 'file size (bytes)'
+TEQC_f_start        = 'start date & time'
+TEQC_f_end          = 'final date & time'
+TEQC_sample_int     = 'sample interval'
+TEQC_miss_epochs    = 'possible missing epochs'
+TEQC_sta_id         = '4-char station code'
+TEQC_sta_name       = 'station name'
+TEQC_sta_sn         = 'station ID number'
+TEQC_ant_sn         = 'antenna ID number'
+TEQC_ant_type       = 'antenna type'
+TEQC_lat            = 'antenna latitude (deg)'
+TEQC_lon            = 'antenna longitude (deg)'
+TEQC_elev           = 'antenna elevation (m)'
+TEQC_ant_height     = 'antenna height (m)'
+TEQC_rcx_sn         = 'receiver ID number'
+TEQC_rcx_type       = 'receiver type'
+TEQC_rcx_firm       = 'receiver firmware'
+
+TEQC_date_format    = '%Y-%m-%d %H:%M:%S.%f'
+
